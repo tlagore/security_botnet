b505c0357e2af5b8a1b6e7f04dbcb0143f23519b
tyrone.lagore <tyrone.lagore@zone46-wc.cs.ucalgary.ca>
2017-03-30 17:08:06 -0600

    converted bot.py to a class based implementation. Added some logging socket receive/send functions to make debugging easier. Bot can now handle parsing multiple messages received in 1 socket read by splitting them based on the irc delimiter. Bot can handle a ping style message and has a dummy implementation to handle a bad user name error message. Currently just hardcodes the name to a new name and tries again, but on multiple fails will try the same name. This will be randomized in the future.

--- bot/bot.py
+++ bot/bot.py
@@ -1,38 +1,98 @@
 import socket
 import time
-
-HOST = "irc.twitch.tv"              # the Twitch IRC server
-PORT = 6667                         # always use port 6667!
-NICK = "someonespecialll"            # your Twitch username, lowercase
-PASS = "oauth:wv4u9nem3b330zkh4kvdjobvjn9e2x" # your Twitch OAuth token
-CHAN = "#someonespecialll"                   # the channel you want to join
+import threading
 
 
-def parsemsg(s):
-    """Breaks a message from an IRC server into its prefix, command, and arguments.
-    """
-    prefix = ''
-    trailing = []
-    if not s:
-       raise IRCBadMessage("Empty line.")
-    if s[0] == ':':
-        prefix, s = s[1:].split(' ', 1)
-    if s.find(' :') != -1:
-        s, trailing = s.split(' :', 1)
-        args = s.split()
-        args.append(trailing)
-    else:
-        args = s.split()
-    command = args.pop(0)
-    return prefix, command, args
+class Bot:
+    def __init__(self, server, name, channel, port):
+        """ """
+        self._server = server
+        self._nick = name
+        self._channel = channel
+        self._port = port
+
+        self._socket = socket.socket()
+        self._socket.connect((server, port))
+        
+        self.doWork()    
+                 
+    def parsemsg(self, s):
+        """Breaks a message from an IRC server into its prefix, command, and arguments.
+        """
+        prefix = ''
+        trailing = []
+        if not s:
+            raise IRCBadMessage("Empty line.")
+        if s[0] == ':':
+            prefix, s = s[1:].split(' ', 1)
+        if s.find(' :') != -1:
+            s, trailing = s.split(' :', 1)
+            args = s.split()
+            args.append(trailing)
+        else:
+            args = s.split()
+            
+        command = args.pop(0)
+            
+        return prefix, command, args
+
+    def logSend(self, msg):
+        print("sending: " + msg)
+        self._socket.send(msg)
+        
+    def logRecv(self):
+        response = self._socket.recv(1024).decode("utf-8")
+        print("received: " + response)
+        responses = response.split("\r\n")
+        return responses[:len(responses) - 1]
+
+    def initConnection(self):
+        self.logSend("NICK {}\r\n".format(self._nick).encode("utf-8"))
+
+        ##change this to be a randomized name per bot, note nick is their identifying feature,
+        ## not the name
+        self.logSend("USER {0} 0 * :tyriggitybiggity".format(self._nick).encode("utf-8"))
+        self.logSend("JOIN {}\r\n".format(self._channel).encode("utf-8"))
+
+    def debugLogResponse(self, prefix, command, args):
+        """ """
+        print("prefix: {0}".format(prefix))
+        print("command: {0}".format(command))
+        print("args: {0}".format(args))
 
 
-# network functions go here
+    def doWork(self):
+        """ """
+        self.initConnection()
 
-s = socket.socket()
-s.connect((HOST, PORT))
-s.send("PASS {}\r\n".format(PASS).encode("utf-8"))
-s.send("NICK {}\r\n".format(NICK).encode("utf-8"))
+        responses = self.logRecv()
+        while responses:
+            for response in responses:
+                prefix, command, args = self.parsemsg(response)
+
+                self.debugLogResponse(prefix, command, args)
+                
+                if command == "PING":
+                    self.logSend("PONG {}\r\n".format(args[0].encode("utf-8")))
+                elif command == "433":
+                    #username taken
+                    self.pickNewName()
+                    self.initConnection()
+            
+
+            responses = self.logRecv()
+
+    def pickNewName(self):
+        """ set new name """
+        #change this to a new server
+        self._name = "butts"
+
+
+if __name__ == "__main__":
+    bot = Bot("127.0.0.1", "tyrone", "#channel1", 6667)
+
+
+'''
 s.send("JOIN {}\r\n".format(CHAN).encode("utf-8"))
 
 print("Connected to IRC...")
@@ -57,3 +117,4 @@ while True:
         print(response)
 s.close()
 #sleep(1 / cfg.RATE)
+'''
